9a7fa59cf8dbd7df05c9ee034ac749c2c82f42a4
doddyph <doddee.ph@gmail.com>
2024-11-03 15:28:20 +0700

    Add web client DEBUG logging

--- src/main/java/id/laris/LarisAssistant/service/flowiseai/FlowiseAiService.java
+++ src/main/java/id/laris/LarisAssistant/service/flowiseai/FlowiseAiService.java
@@ -2,13 +2,17 @@ package id.laris.LarisAssistant.service.flowiseai;
 
 import id.laris.LarisAssistant.service.flowiseai.dto.FlowiseAiRequest;
 import id.laris.LarisAssistant.service.flowiseai.dto.FlowiseAiResponse;
+import io.netty.handler.logging.LogLevel;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Log4j2
 @Service
@@ -23,7 +27,11 @@ public class FlowiseAiService {
     private String predictionId;
 
     public FlowiseAiService(WebClient.Builder builder) {
-        this.webClient = builder.baseUrl("http://117.53.144.144:3000").build();
+        this.webClient = builder.baseUrl("http://117.53.144.144:3000")
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                        .wiretap("reactor.netty.http.client.HttpClient",
+                                LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL)))
+                .build();
     }
 
     public Mono<String> prediction(String question) {
